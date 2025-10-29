83a369f3964737e3867f70e930497a6cbfb523a2
Martin Čížek <officialpatera6@gmail.com>
2025-10-29 11:43:08 +0100

    Strategy pattern implementation

---
+++ src/main/java/cizek/anv/cafe/CardPaymentStrategy.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cizek.anv.cafe;
+
+/**
+ *
+ * @author DELL
+ */
+public class CardPaymentStrategy implements PaymentStrategy {
+
+    @Override
+    public String pay(double amount, int table) {
+        return "Customer wants to pay " + amount + " using Credit Card at table " + table;
+    }
+    
+}
