81029cf4e09d12c3357d514be605a263cc093172
Martin Čížek <officialpatera6@gmail.com>
2025-11-19 11:33:17 +0100

    command pattern implementation

--- src/main/java/cizek/anv/cafe/ANVCafe.java
+++ src/main/java/cizek/anv/cafe/ANVCafe.java
@@ -13,35 +13,30 @@ public class ANVCafe {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         
-        CafeConfig config = CafeConfig.getInstance("Smart Cafe");
-//        System.out.println(config.getCafeName());
-        
-        Drink t = DrinkFactory.createDrink("tea");
-        System.out.println(t.serve());
-        
-        Drink c = DrinkFactory.createDrink("coffee");
-        System.out.println(c.serve());
-        
         EmployeeObserver waiter = new EmployeeObserver("Waiter");
         EmployeeObserver barista = new EmployeeObserver("Barista");
-        
         OrderSubject order = new OrderSubject();
         order.addObserver(barista);
-        order.addObserver(waiter);
-        
-        CustomDrink cd = new CustomDrink.Builder("coffee").milk().sugar().build();
-        //System.out.println(cd);
-        String msg = cd + " in " + config.getCafeName();
-        //String msg=null;
-        
-        order.notifyAll(msg);
-        
         OrderSubject payment = new OrderSubject();
         payment.addObserver(waiter);
         
+        CustomDrink coffee = new CustomDrink.Builder("coffee").milk().sugar().build();
+        CustomDrink tea = new CustomDrink.Builder("tea").sugar().build();
+        
+        CafeConfig config = CafeConfig.getInstance("Smart Cafe");
+        String cafeName = config.getCafeName();
+        
+        Command coffeeOrder = new OrderCommand(order, coffee + " in " + cafeName);
+        Command teaOrder = new OrderCommand(order,tea + " in " + cafeName);
+        
+        coffeeOrder.execute();
+        teaOrder.execute();
+        
         Checkout checkout = new Checkout(new CardPaymentStrategy());
-        payment.notifyAll(checkout.processPayment(150, 4));
+        Command payByCard = new PaymentCommand(payment,checkout,150,4);
         checkout.setStrategy(new CashPaymentStrategy());
-        payment.notifyAll(checkout.processPayment(100, 2));
+        Command payByCash = new PaymentCommand(payment,checkout,100,2);
+        payByCard.execute();
+        payByCash.execute();
     }
 }
